02b8e35304e2bc57191b2874a72cf710e4135bf3
Zanti01121995 <santhoshsekar788@gmail.com>
2023-11-20 01:02:12 +0530

    new

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const mongoUrl = "http://localhost:3000/api";
+// export const mongoUrl = "http://localhost:3000/api";
+export const mongoUrl = import.meta.env.REACT_APP_BACKEND_URL || "https://hallbooking-oeya.onrender.com/api"
